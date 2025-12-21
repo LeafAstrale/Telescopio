0bf423343e21b83f0604c597093581f02ba8a60f
Astrale <samuel.950@hotmail.com>
2025-12-21 13:13:35 -0500

    Pre-funcionamiento- Establecimiento de método de escritura

---
+++ src/Functions/Filehandler.ts
@@ -0,0 +1,21 @@
+import { create, BaseDirectory, FileHandle } from '@tauri-apps/plugin-fs'
+
+export async function crearArchivo(filename: string, cuerpo: string) {
+  let archivo: FileHandle | null = null
+
+  try {
+    archivo = await create(`${filename}.txt`, {
+      baseDir: BaseDirectory.AppData
+    })
+
+    const data = new TextEncoder().encode(cuerpo);
+    await archivo.write(data);
+
+  } catch (e) {
+    console.log(e + "wea")
+  } finally {
+    if (archivo) {
+      await archivo.close()
+    }
+  }
+}
